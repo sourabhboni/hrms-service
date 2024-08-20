bf3cf2f08f5e28df2c2588bc282cd21403a9851a
sourabhboni <bonisourabh@gmail.com>
2024-08-20 15:50:00 +0100

    Defined routes for employee creation and retrieval

---
+++ routes/employeeRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { createEmployee, getEmployees } = require('../controllers/employeeController');
+
+const router = express.Router();
+
+router.post('/create', createEmployee);
+router.get('/', getEmployees);
+
+module.exports = router;
